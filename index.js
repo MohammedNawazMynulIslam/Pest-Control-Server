2191ab66a7a989ae67dc34c0282b3935a5c35e00
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-05 21:19:10 +0600

    added services data

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jyxnen.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const serviceCollection = client.db('PestControl').collection('services')
+
+    // get services
+    app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Pest control server')
+})
+
+app.listen(port, () => {
+  console.log(`Serving is running listening on port ${port}`)
+})
