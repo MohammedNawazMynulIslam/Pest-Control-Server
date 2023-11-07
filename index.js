6fdd712afb7715e8ee2a9512fcb382bea138d353
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-07 21:56:12 +0600

    added cookie parser to store token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const jwt =require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -7,8 +8,12 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 // user email
 
