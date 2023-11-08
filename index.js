c597f0b2d842eaf98e91425ed6b2948dc9bdacb9
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-08 10:57:14 +0600

    added sameSite true secure true

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ async function run() {
             res
             .cookie("token",token,{
               httpOnly: true,
-              secure:false,
+              secure:true,
               sameSite:"none",
               expires:expirationDate,
             })
